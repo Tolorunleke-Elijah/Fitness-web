6f4aa2b96195b94a70a0c493831c06217a27f8a3
Elijah <ctolorunleke@yahoo.com>
2024-08-07 12:33:13 +0100

    fitness

---
+++ js/body index.js
@@ -0,0 +1,6 @@
+alert("what is your mass in kg")
+let mass = parseInt(prompt(""));     
+alert("what is your height in meters")
+let height = parseInt(prompt("")**2);
+let body_index = mass/height;
+alert(`Your body index is ${body_index}`)
